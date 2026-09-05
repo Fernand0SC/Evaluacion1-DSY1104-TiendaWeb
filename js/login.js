1468edb158f164ea2cc4d98846eb5b2555039589
Lokomati <matia.munozo@duocuc.cl>
2026-09-05 19:50:20 -0400

    La validacion del correo electronico

---
+++ js/login.js
@@ -0,0 +1,31 @@
+document.getElementById('formLogin').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    let isValid = true;
+
+    const correoInput = document.getElementById('correo').value;
+    const errorCorreo = document.getElementById('errorCorreo');
+    const regexCorreo = /^[a-zA-Z0-9._-]+@(duoc\.cl|profesor\.duoc\.cl|gmail\.com)$/;
+
+    if (!correoInput) {
+        errorCorreo.textContent = "El correo es requerido."; errorCorreo.style.display = "block"; isValid = false;
+    } else if (!regexCorreo.test(correoInput)) {
+        errorCorreo.textContent = "Solo correos @duoc.cl, @profesor.duoc.cl o @gmail.com."; errorCorreo.style.display = "block"; isValid = false;
+    } else {
+        errorCorreo.style.display = "none";
+    }
+
+    const passwordInput = document.getElementById('password').value;
+    const errorPassword = document.getElementById('errorPassword');
+
+    if (!passwordInput) {
+        errorPassword.textContent = "La contraseña es requerida."; errorPassword.style.display = "block"; isValid = false;
+    } else if (passwordInput.length < 4 || passwordInput.length > 10) {
+        errorPassword.textContent = "La contraseña debe tener entre 4 y 10 caracteres."; errorPassword.style.display = "block"; isValid = false;
+    } else {
+        errorPassword.style.display = "none";
+    }
+
+    if (isValid) {
+        window.location.href = "../admin/admin-home.html"; // Redirige al panel de administración
+    }
+});
