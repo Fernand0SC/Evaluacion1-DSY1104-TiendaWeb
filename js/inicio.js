3345ec7e509d0cd1dd13becafd5a2fa849cbd1fc
Fernand0SC <fe.serranoc@duocuc.cl>
2026-09-06 17:32:39 -0300

    implementacion de localStorage

--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,26 @@
-function agregarAlCarrito() {
+function agregarAlCarrito(nombre, precio) {
+
+    let carrito = [];
+    const memoria = localStorage.getItem('miCarrito');
+    if (memoria) {
+        carrito = JSON.parse(memoria);
+    }
+
+    const productoExistente = carrito.find(item => item.nombre === nombre);
+
+    if (productoExistente) {
+        productoExistente.cantidad += 1;
+        productoExistente.subtotal = productoExistente.cantidad * productoExistente.precio;
+    } else {
+        carrito.push({
+            nombre: nombre,
+            precio: precio,
+            cantidad: 1,
+            subtotal: precio
+        });
+    }
+
+    localStorage.setItem('miCarrito', JSON.stringify(carrito));
+
     alert('¡Producto añadido al carrito con éxito!');
 }
